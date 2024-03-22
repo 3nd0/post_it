18e211c6eb8ef7f33c8e8921fbab9648111eb674
wataru.endo <endo0430w@gmail.com>
2024-03-23 03:18:52 +0900

    Create Board components

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,9 @@
+const Board = (): JSX.Element => {
+  return (
+    <div className="w-64 h-48 rounded bg-zinc-800/75 hover:bg-zinc-700/75">
+      hello world
+    </div>
+  )
+}
+
+export default Board
