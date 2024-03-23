c96f83d53b9debb9d28180bce3d782b102451708
wataru.endo <endo0430w@gmail.com>
2024-03-23 17:34:00 +0900

    アコーディオンコンポーネントに開閉アイコンを付与

--- src/components/SideBar/Accordion/index.tsx
+++ src/components/SideBar/Accordion/index.tsx
@@ -2,6 +2,7 @@
 
 import { Disclosure } from '@headlessui/react'
 import React from 'react'
+import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
 const Accordion = ({
   subtitle,
@@ -12,16 +13,23 @@ const Accordion = ({
 }): JSX.Element => {
   return (
     <Disclosure>
-      <Disclosure.Button className="flex w-60 px-3 py-1 text-base text-zinc-500 hover:text-zinc-200">
-        {subtitle}
-      </Disclosure.Button>
-      {items.map((item, index) => (
-        <React.Fragment key={index}>
-          <Disclosure.Panel className="flex w-52 ml-6 px-3 py-1 text-base text-zinc-500 hover:text-zinc-200 relative border-s border-zinc-500 hover:border-zinc-200">
-            {item.name}
-          </Disclosure.Panel>
-        </React.Fragment>
-      ))}
+      {({ open }) => (
+        <>
+          <Disclosure.Button className="flex w-full justify-between items-center px-3 py-1 text-base text-zinc-500 hover:text-zinc-200">
+            {subtitle}
+            <ChevronUpIcon
+              className={`w-5 h-5 ${open ? 'rotate-180 transform' : ''}`}
+            />
+          </Disclosure.Button>
+          {items.map((item, index) => (
+            <React.Fragment key={index}>
+              <Disclosure.Panel className="flex w-52 ml-6 px-3 py-1 text-base text-zinc-500 hover:text-zinc-200 relative border-s border-zinc-500 hover:border-zinc-200">
+                {item.name}
+              </Disclosure.Panel>
+            </React.Fragment>
+          ))}
+        </>
+      )}
     </Disclosure>
   )
 }
