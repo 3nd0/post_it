0ee14f23c9445550d8e26d7702b53d171974ea8d
wataru.endo <endo0430w@gmail.com>
2024-03-23 14:57:18 +0900

    Add a sidebar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,14 @@
+import Accordion from './Accordion'
+
 const SideBar = (): JSX.Element => {
-  return <nav className="w-64 h-screen bg-zinc-800/75">side bar</nav>
+  return (
+    <nav className="w-64 h-screen px-3 bg-zinc-800/75">
+      <Accordion
+        subtitle="task"
+        items={[{ name: 'task1' }, { name: 'task2' }, { name: 'task3' }]}
+      />
+    </nav>
+  )
 }
 
 export default SideBar
