3d061de07eb251f80cdc35b30d48d748cdad4ee4
wataru.endo <endo0430w@gmail.com>
2024-03-23 17:55:16 +0900

    Added anchor component for sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,8 +1,10 @@
+import Anchor from './Anchor'
 import Accordion from './Accordion'
 
 const SideBar = (): JSX.Element => {
   return (
     <nav className="w-64 h-screen px-3 bg-zinc-800/75">
+      <Anchor text="top" path="/" />
       <Accordion
         subtitle="task"
         items={[{ name: 'task1' }, { name: 'task2' }, { name: 'task3' }]}
