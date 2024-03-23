23e40bcfdcdbe9cb171d303d9edd8f57cd4bf775
wataru.endo <endo0430w@gmail.com>
2024-03-23 13:28:06 +0900

    アコーディオンにhoverしたらtext colorを変える

--- src/components/SideBar/Accordion/index.tsx
+++ src/components/SideBar/Accordion/index.tsx
@@ -12,12 +12,12 @@ const Accordion = ({
 }): JSX.Element => {
   return (
     <Disclosure>
-      <Disclosure.Button className="flex w-60 px-3 py-1 rounded text-base hover:bg-zinc-700/75">
+      <Disclosure.Button className="flex w-60 px-3 py-1 text-base text-zinc-500 hover:text-zinc-200">
         {subtitle}
       </Disclosure.Button>
       {items.map((item, index) => (
         <React.Fragment key={index}>
-          <Disclosure.Panel className="flex w-52 ml-6 px-3 py-1 text-base text-zinc-500 hover:text-zinc-200 relative border-s border-gray-200">
+          <Disclosure.Panel className="flex w-52 ml-6 px-3 py-1 text-base text-zinc-500 hover:text-zinc-200 relative border-s border-zinc-500 hover:border-zinc-200">
             {item.name}
           </Disclosure.Panel>
         </React.Fragment>
