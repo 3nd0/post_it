3548461cee24173da0168d2c198268a7732732a7
wataru.endo <endo0430w@gmail.com>
2024-03-23 13:23:22 +0900

    アコーディオンのアイテムにstyle適用

--- src/components/SideBar/Accordion/index.tsx
+++ src/components/SideBar/Accordion/index.tsx
@@ -17,7 +17,9 @@ const Accordion = ({
       </Disclosure.Button>
       {items.map((item, index) => (
         <React.Fragment key={index}>
-          <Disclosure.Panel>{item.name}</Disclosure.Panel>
+          <Disclosure.Panel className="flex w-52 ml-6 px-3 py-1 text-base text-zinc-500 hover:text-zinc-200 relative border-s border-gray-200">
+            {item.name}
+          </Disclosure.Panel>
         </React.Fragment>
       ))}
     </Disclosure>
