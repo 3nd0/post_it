befded4519f9fbff58038a9fcb795c2f8d3b1cf2
wataru.endo <endo0430w@gmail.com>
2024-06-08 00:24:29 +0900

    Add sidebar items components

--- src/components/Dashboard/SideBar.tsx
+++ src/components/Dashboard/SideBar.tsx
@@ -1,7 +1,36 @@
+import React from "react"
+import Link from "next/link"
+import type { MenuItems } from "./types/SideBar"
+
+const items: MenuItems[] = [
+    {
+        name: 'item1',
+        path: '#'
+    },
+    {
+        name: 'item2',
+        path: '#'
+    }
+]
+
 const SideBar = () => {
     return (
         <aside className="fixed top-0 left-0 mt-20 w-64 h-screen z-40 bg-white border-r border-inherit">
-            <div className="h-full px-3 bg-white overflow-y-auto"></div>
+            <div className="h-full px-3 py-4 bg-white overflow-y-auto">
+                <ul className="space-y-2 font-medium">
+                    {
+                        items.map((item, index) => (
+                            <React.Fragment key={index}>
+                                <li>
+                                    <Link href={item.path} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100">
+                                        <span className="ms-3">{item.name}</span>
+                                    </Link>
+                                </li>
+                            </React.Fragment>
+                        ))
+                    }
+                </ul>
+            </div>
         </aside>
     )
 }
